0e4f16650cfa2889b7e97846473cf8a71d3bb08c
adityakhode <aditya_khode_entc@moderncoe.edu.in>
2025-11-25 09:55:08 +0530

    S3

---
+++ src/main/java/com/AduProjects/FileMover/config/s3Config.java
@@ -0,0 +1,30 @@
+package com.AduProjects.FileMover.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+@Configuration
+public class s3Config {
+    @Value("${cloud.aws.credentials.assess-key}")
+    private String assessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+    @Bean
+    public S3Client s3Client(){
+
+        AwsBasicCredentials awsBasicCredentials = AwsBasicCredentials.create(assessKey, secretKey);
+        return S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(awsBasicCredentials))
+                .build();
+    }
+}
